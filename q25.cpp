b906e2a7065d23d7752b32d1eed27ea56fbb235d
Aryan Sethi <aryan.rdps@gmail.com>
2022-03-08 17:05:12 +0530

    Q25 and 26 done

---
+++ q25.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+class Student
+{
+    int roll, year;
+    float total_marks;
+    char name[30];
+    char class_name[30];
+
+public:
+    Student()
+    {
+        roll = 0;
+        year = 0;
+        total_marks = 0;
+        strcpy(name, "no name");
+        strcpy(class_name, "no class");
+    }
+
+    void inputStudent()
+    {
+        cout << "Enter roll: ";
+        cin >> roll;
+        cout << "Enter year: ";
+        cin >> year;
+        cout << "Enter total marks: ";
+        cin >> total_marks;
+        cout << "Enter name: ";
+        cin.ignore();
+        cin.getline(name, 29);
+        cout << "Enter class: ";
+        cin.ignore();
+        cin.getline(class_name, 29);
+    }
+
+    void outputStudent()
+    {
+        cout << roll << " " << name << " " << total_marks << endl;
+    }
+
+    void storeInFile()
+    {
+        ofstream out("student.dat", ios::app | ios::binary);
+        out.write((char *)this, sizeof(*this));
+        out.close();
+    }
+
+    void getStudents()
+    {
+        ifstream inp("student.dat", ios::binary);
+
+        inp.read((char *)this, sizeof(*this));
+        while (!inp.eof())
+        {
+            outputStudent();
+            inp.read((char *)this, sizeof(*this));
+        }
+
+        inp.close();
+    }
+};
+
+int main()
+{
+
+    Student s;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Details of student: " << i + 1 << endl;
+        s.inputStudent();
+        s.storeInFile();
+    }
+
+    return 0;
+}
